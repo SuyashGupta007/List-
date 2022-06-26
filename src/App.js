1e330abe8231d9f985236a8d70cfe67138366784
SuyashGupta007 <suyashgupta34604@gmail.com>
2022-06-26 18:04:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import List from './components/List';
+import React from 'react'
 import './App.css';
 
 function App() {
+  let shopping = [
+    {
+      Item_Name: "Nord Stage 3 88 Keys",
+      Item_Price: "$5,738"
+    },
+    {
+      Item_Name: "Line6 Helix Rack Guitar Processor -1x",
+      Item_Price: "$1,831.57"
+    },
+    {
+      Item_Name: "Nord Lead A1",
+      Item_Price: "$1,700"
+    },
+    {
+      Item_Name: "Moog Subsequent 37",
+      Item_Price: "$1,700"
+    },
+    {
+      Item_Name: "Moog Minitaure 2010s Black",
+      Item_Price: "$682.80"
+    },
+
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      {shopping.map((shopping_items, index) => {
+        return <List item={shopping_items} key={index} />
+      })}
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const List = (props) => {
+    console.log(props);
+    return (
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 ">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50  ">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            {props.item.Item_Name}
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Color
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Category
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            {props.item.Item_Price}
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900  whitespace-nowrap">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td class="px-6 py-4">
+                            Sliver
+                        </td>
+                        <td class="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td class="px-6 py-4">
+                            $2999
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="#" class="font-medium text-blue-600  hover:underline">Edit</a>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+    )
+}
+
+export default List
+
